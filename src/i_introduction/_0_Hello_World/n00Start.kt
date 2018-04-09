2663a5c4d3d4f1268a9966334eb2693dd59ffbea
Allan Davis <cajun.code@gmail.com>
2018-04-09 15:08:57 -0400

    Solved first Koan

--- src/i_introduction/_0_Hello_World/n00Start.kt
+++ src/i_introduction/_0_Hello_World/n00Start.kt
@@ -22,8 +22,9 @@ fun todoTask0(): Nothing = TODO(
     """,
     documentation = doc0(),
     references = { task0(); "OK" }
+    
 )
-
-fun task0(): String {
-    return todoTask0()
-}
+fun task0() = "OK"
+// fun task0(): String {
+//     return todoTask0()
+// }
